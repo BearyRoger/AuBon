3e517ae983e989b153bfacd71d88c1ceb3491f34
matheohero <134490234+matheohero@users.noreply.github.com>
2023-12-12 10:23:27 +0100

    Add files via upload

--- ChambresDhôtes.php
+++ ChambresDhôtes.php
@@ -3,14 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="mainCss.css" rel="stylesheet" />
     <title>Chambres d'hôtes</title>
 </head>
 <body>
-        <div>
-            <h1>Nos chambre d'hôtes</h1>
-        </div>
-
-        <div>
+    <div>
+        <h1>Nos chambre d'hôtes</h1>
+    </div>
+    <div class='divGrid'>
+        <div class='one'>
             <h2>1er Chambre</h2>
             <p>Surface : 50 m²</p>
             <p>Prix : 350 € / nuit</p>
@@ -18,7 +19,7 @@
             <img src="https://www.gites-de-france-gironde.com/img/liste/chambre-hotes-1615279251.jpg">
             <p>Description : Chambre modeste pour 3 personne (1 lit double et 1 lit simple)</p>
         </div>
-        <div>
+        <div class='two'>
             <h2>2éme Chambre</h2>
             <p>Surface : 200 m²</p>
             <p>Prix : 849 € / nuit</p>
@@ -42,5 +43,6 @@
             <img src="https://www.gites-de-france-gironde.com/img/liste/chambre-hotes-1615279251.jpg">
             <p>Description : Petite chambre pour 3 personne (1 lit double et 1 lit simple)</p>
         </div>
+</div>
 </body>
 </html>
