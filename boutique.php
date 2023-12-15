ae46d00d9d380eb586c5fc2d4732cf4104fa0188
Hwelf Kyx <134490297+KylLandais@users.noreply.github.com>
2023-12-15 09:40:42 +0100

    add carousel boutique

--- boutique.php
+++ boutique.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chambres d'hôtes</title>
     <link rel="stylesheet" href="mainCss.css">
+    <link rel="stylesheet" href="carousel.css">
+    <script src="carousel.js" async></script>
 </head>
 <body>
     <div class="divGrid">
@@ -19,11 +21,63 @@
 
         <div class="three">
             <h2>Fruits et légumes :</h2>
-
+            <div class="container">
+                <div class="carousel">
+                    <div class="carousel-inner">
+                        <div class="slide">
+                            <img src="feet.jpg"
+                                alt="Image 1">
+                        </div>
+                        <div class="slide">
+                            <img src="Zle.jpg"
+                                alt="Image 2">
+                        </div>
+                        <div class="slide">
+                            <img src="https://images.pexels.com/photos/3183132/pexels-photo-3183132.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                                alt="Image 3">
+                        </div>
+                    </div>
+                    <!-- Conteneur pour les boutons de navigation -->
+                    <div class="carousel-controls">
+                        <!-- Bouton pour passer à la diapositive précédente -->
+                        <button id="prev">Précédent</button>
+                        <!-- Bouton pour passer à la diapositive suivante -->
+                        <button id="next">Suivant</button>
+                    </div>
+                    <!-- Conteneur pour les points de navigation -->
+                    <div class="carousel-dots"></div>
+                </div>
+                </div>
         </div>
         <div class="four">
             <h2>Viandes :</h2>
-            
+            <div class="container">
+            <div class="carousel">
+                    <div class="carousel-inner">
+                        <div class="slide">
+                            <img src="feet.jpg"
+                                alt="Image 1">
+                        </div>
+                        <div class="slide">
+                            <img src="Zle.jpg"
+                                alt="Image 2">
+                        </div>
+                        <div class="slide">
+                            <img src="https://images.pexels.com/photos/3183132/pexels-photo-3183132.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                                alt="Image 3">
+                        </div>
+                    </div>
+                    <!-- Conteneur pour les boutons de navigation -->
+                    <div class="carousel-controls">
+                        <!-- Bouton pour passer à la diapositive précédente -->
+                        <button id="prev">Précédent</button>
+                        <!-- Bouton pour passer à la diapositive suivante -->
+                        <button id="next">Suivant</button>
+                    </div>
+                    <!-- Conteneur pour les points de navigation -->
+                    <div class="carousel-dots"></div>
+                </div>
+                </div>
         </div>
     </div>
 </body>
