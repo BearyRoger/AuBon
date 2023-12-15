39bfa3c331da0a7f06dac4a6ca211fb58c31f468
Eowynchi <148531019+Eowynchi@users.noreply.github.com>
2023-12-15 10:39:07 +0100

    Update NosValeur.php
    fautes d'ortographes corrigées

--- NosValeur.php
+++ NosValeur.php
@@ -10,11 +10,11 @@
         <h1>Nos valeurs</h1>
     </div>
     <div>
-        - On vous garantie un séjour de qualiter<br>
+        - On vous garantie un séjour de qualité<br>
         - A un prix abordable<br>
-        - On sera vous guider afin de vous permétre de passer un agréable sèjour<br>
-        - Notre restoration est l'une des meilleurs de la région avec 4.9/5 sur foodAdvisor<br>
-        - Plus de 95% de nos client sont satisfait et on vous promet que vous en feriez partie<br>
+        - On saura vous guider afin de vous permettre de passer un agréable séjour<br>
+        - Notre restauration est l'une des meilleurs de la région avec 4.9/5 sur foodAdvisor<br>
+        - Plus de 95 % de nos clients sont satisfaits, et nous vous promettons que vous en ferez partie.<br>
     </div>
 </body>
 </html>
