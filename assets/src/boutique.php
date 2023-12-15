3815c0dde5a1d0b9f5abc65254e6305ddedca0b1
Hwelf Kyx <134490297+KylLandais@users.noreply.github.com>
2023-12-15 12:02:12 +0100

    final boutique

---
+++ assets/src/boutique.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chambres d'hôtes</title>
+    
+    <link rel="stylesheet" href="carousel1.css">
+    <link rel="stylesheet" href="carousel2.css">
+    <script src="carousel1.js" async></script>
+    <script src="carousel2.js" async></script>
+</head>
+<body>
+    <div class="divGrid">
+        <div class='one'>
+            <h1>Notre Boutique</h1>
+        </div>
+
+        <div class='two'>
+            <h3>Vous trouverez dans cette boutique tout type de produits <br> tous sont produits localement dans notre ferme</h3><br>
+            <p></p>
+        </div>
+
+        <div class="three">
+            <div class="container1">
+            <h2>Fruits et légumes :</h2>
+                <div class="carousel1">
+                    <div class="carousel1-inner">
+                        <div class="slide1">
+                            <img src="téléchargement.jpg" alt="Image 1">
+                        </div>
+                        <div class="slide1">
+                            <img src="téléchargement (1).jpg"alt="Image 2">
+                        </div>
+                        <div class="slide1">
+                            <img src="téléchargement (2).jpg"alt="Image 3">
+                        </div>
+                    </div>
+                    <!-- Conteneur pour les boutons de navigation -->
+                    <div class="carousel1-controls">
+                        <!-- Bouton pour passer à la diapositive précédente -->
+                        <button id="prev1">Précédent</button>
+                        <!-- Bouton pour passer à la diapositive suivante -->
+                        <button id="next1">Suivant</button>
+                    </div>
+                    <!-- Conteneur pour les points de navigation -->
+                    <div class="carousel1-dots"></div>
+                </div>
+                </div>
+        </div>
+        <div class="four">
+        <div class="container2">
+            <h2>Viandes :</h2>
+            <div class="carousel2">
+                    <div class="carousel2-inner">
+                        <div class="slide2">
+                            <img src="téléchargement (3).jpg"alt="Image 1">
+                        </div>
+                        <div class="slide2">
+                            <img src="téléchargement (4).jpg"alt="Image 2">
+                        </div>
+                        <div class="slide2">
+                            <img src="téléchargement (5).jpg"alt="Image 3">
+                        </div>
+                    </div>
+                    <!-- Conteneur pour les boutons de navigation -->
+                    <div class="carousel2-controls">
+                        <!-- Bouton pour passer à la diapositive précédente -->
+                        <button id="prev2">Précédent</button>
+                        <!-- Bouton pour passer à la diapositive suivante -->
+                        <button id="next2">Suivant</button>
+                    </div>
+                    <!-- Conteneur pour les points de navigation -->
+                    <div class="carousel2-dots"></div>
+                </div>
+                </div>
+        </div>
+    </div>
+</body>
+</html>
